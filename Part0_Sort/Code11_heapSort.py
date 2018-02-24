aa882f2fd99d9b13091ac0c97a863e5cd82e135b
lianyingteng <lianyingteng@hotmail.com>
2018-02-24 21:34:02 +0800

    Update Code11_heapSort.py

--- Part0_Sort/Code11_heapSort.py
+++ Part0_Sort/Code11_heapSort.py
@@ -4,50 +4,43 @@
 def getMinKNumsByHeap(arr, k):
 	"""通过建立k个元素大根堆 找到无序数组中 前k个最小的数
 	"""
-	if k < 1 or k > len(arr):
-		return None
+	if arr == None or k <= 0 or len(arr) <= k:
+		return arr
 
-	KHeap = [0] * k
-	for i in range(k):
-		heapInsert(KHeap, arr[i], i)
+	res = [arr[0]]
+	for i in range(1, k):
+		res.append(arr[i])
+		insertHeap(res, i)
 
 	for i in range(k, len(arr)):
-		if arr[i] < KHeap[0]:
-			KHeap[0] = arr[i]
-			heapity(KHeap, 0, k)
+		if res[0] > arr[i]:
+			res[0] = arr[i]
+			heapify(res, 0, k)
 
-	return KHeap
+	return res
 
-def heapInsert(arr, val, index):
-	"""向堆中插入元素， 然后将其调整为大根堆
-	"""
-	arr[index] = val
 
-	while index != 0:  ####
-		parent = (index-1) // 2
+def insertHeap(arr, index):
+	parent = (index-1) >> 1
+	while parent >= 0 and arr[parent] < arr[index]:
+		arr[parent], arr[index] = arr[index], arr[parent]
+		index = parent
+		parent = (index-1) >> 1
 
-		if arr[parent] < arr[index]:
-			arr[parent], arr[index] = arr[index], arr[parent]
-			index = parent
-		else:
+
+def heapify(arr, index, length):
+
+	left = 2 * index + 1
+	while left < length:
+		largest = left+1 if left+1 < length and arr[left+1] > arr[left] else left
+		largest = index if arr[index] >= arr[largest] else largest
+
+		if largest == index:
 			break
 
-def heapity(arr, index, heapSize):
-	"""堆化
-	"""
-	lchild = 2 * index + 1
-	rchild = 2 * index + 2
-
-	while lchild < heapSize:
-		largest = rchild if rchild < heapSize and arr[rchild] > arr[lchild] else lchild
-		if 	arr[largest] > arr[index]:
-			arr[largest], arr[index] = arr[index], arr[largest]
-			index = largest
-
-			lchild = 2 * index + 1
-			rchild = 2 * index + 2
-		else:
-			break
+		arr[index], arr[largest] = arr[largest], arr[index]
+		index = largest
+		left = 2 * index + 1
 
 
 
